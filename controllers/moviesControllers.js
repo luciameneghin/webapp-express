64cb7a2648dd5e2e5d095e43c3ffceca4b7c5141
luciameneghin <lucia.meneghin00@gmail.com>
2025-02-06 14:47:17 +0100

    feat: added index route

--- controllers/moviesControllers.js
+++ controllers/moviesControllers.js
@@ -1,8 +1,17 @@
 const connection = require('../data/db')
 
+
 const index = (req, res) => {
-  res.send('index funzionante')
-}
+  const sql = 'SELECT * FROM movies';
+
+  connection.query(sql, (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: 'Errore nella query del database', err });
+    }
+    console.log(req.body);
+    res.json(results);
+  });
+};
 
 const show = (req, res) => {
   const id = req.params.id;
