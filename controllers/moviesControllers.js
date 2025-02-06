bf4f306f0b921dc18f82561dccb863b2bde0d69a
luciameneghin <lucia.meneghin00@gmail.com>
2025-02-06 14:56:57 +0100

    feat: adeed show route

--- controllers/moviesControllers.js
+++ controllers/moviesControllers.js
@@ -15,7 +15,27 @@ const index = (req, res) => {
 
 const show = (req, res) => {
   const id = req.params.id;
-  res.send(`film trovato`)
+  const sqlMovies = 'SELECT * FROM movies WHERE id = ?'
+  const sqlReviews = `SELECT * 
+  FROM reviews R
+  WHERE R.movie_id = ?`
+
+  connection.query(sqlMovies, [id], (e, results) => {
+    if (e) {
+      return res.status(500).json({ error: 'Errore nella query del database', err });
+    }
+    if (results.length === 0) {
+      return res.status(404).json({ error: 'post non trovato' })
+    }
+
+    let movie = results[0]
+
+    connection.query(sqlReviews, [id], (e, reviewsResaults) => {
+      if (e) return res.status(500).json({ error: 'Errore nella query del database', err });
+      movie.reviews = reviewsResaults;
+      res.json(movie);
+    })
+  })
 }
 
 module.exports = {
